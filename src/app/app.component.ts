666e28edda8f7537e0443e766dc7fd5dad7081d8
Sylvain Hugues YOO <sylvain.hugues@yoo.digital>
2025-11-12 18:35:03 +0100

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DemoComponent],
+  imports: [DemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
