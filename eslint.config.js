947d6b1f7eed196816480e7db28911728416312a
Sylvain Hugues YOO <sylvain.hugues@yoo.digital>
2025-12-02 18:00:44 +0100

    works

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,36 @@
 import angularEslintPlugin from "@angular-eslint/eslint-plugin";
 import angularEslintTemplatePlugin from "@angular-eslint/eslint-plugin-template";
 import angularTemplateParser from "@angular-eslint/template-parser";
+import { FlatCompat } from "@eslint/eslintrc";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 import yooAngularPlugin from "@yoo-digital/eslint-plugin-angular";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
+  ...compat.extends("@yoo-digital/eslint-config-base").map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+  })),
+  ...compat.extends("@yoo-digital/eslint-config-angular").map(config => ({
+    ...config,
+    files: config.files || ["**/*.ts"],
+  })),
   {
     files: ["**/*.ts"],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
         project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
         sourceType: "module",
       },
     },
